5fefa5e8a36b932894b7d4a2970b3a9cffff4264
David Fertitta <Davidj.fertitta@gmail.com>
2023-02-03 16:13:14 -0600

    encryption working

--- src/Apis/authentication.js
+++ src/Apis/authentication.js
@@ -1,10 +1,16 @@
 import { addDoc, collection } from '@firebase/firestore';
 import { fireDB } from '../firebaseConfig';
+import CryptoJS from 'crypto-js';
 
 export const LoginUser = (payload) => {};
 
 export const RegisterUser = async (payload) => {
     try {
+        const encryptedPassword = CryptoJS.AES.encrypt(
+            payload.password,
+            'Handi-Stall'
+        ).toString();
+        payload.password = encryptedPassword;
         const response = await addDoc(collection(fireDB, 'users'), payload);
         return {
             success: true,
