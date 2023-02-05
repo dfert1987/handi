8a0e16963a39308797f4e4aa6a3d41d37cf1c606
David Fertitta <Davidj.fertitta@gmail.com>
2023-02-04 23:12:07 -0600

    protected route set up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import { Routes, Route } from 'react-router-dom';
 import LoginPage from './Views/LoginPage/LoginPage';
 import { HomePage } from './Views/HomePage/HomePage';
 import './App.css';
+import ProtectedRoute from './Components/ProtectedRoute';
+import PublicRoute from './Components/PublicRoute';
 
 export default function App() {
     return (
         <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/login' element={<LoginPage />} />
+            <Route
+                path='/'
+                element={
+                    <ProtectedRoute>
+                        <HomePage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path='/login'
+                element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                }
+            />
         </Routes>
     );
 }
