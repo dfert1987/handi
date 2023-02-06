a007051b73954acf15b6b5d9c55b1c7a13ff8b81
David Fertitta <Davidj.fertitta@gmail.com>
2023-02-06 09:13:38 -0600

    files and routes in place for navigation

--- src/Components/DefaultLayout.js
+++ src/Components/DefaultLayout.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Logo from '../Assets/Images/logogator.png';
 import '../Styles/layout.css';
 
 const DefaultLayout = ({ children }) => {
     const user = JSON.parse(localStorage.getItem('user'));
+    const navigate = useNavigate();
+    const userMenu = [
+        {
+            title: 'Home',
+            onClick: () => navigate('/'),
+            icon: <i class='ri-home-3-line'></i>,
+        },
+        {
+            title: 'Profile',
+            onClick: () => navigate()
+        }
+    ];
     return (
         <div className='layout'>
             <div className='sidebar justify-content-between'>
-                <span>GR</span>
+                <div className='menu'></div>
             </div>
             <div className='content'>
                 <div className='header'>
                     <span className='top-bar-title'>
-                        <img className='top-logo' src={Logo} alt='Gator Rater Logo' />
+                        <img
+                            className='top-logo'
+                            src={Logo}
+                            alt='Gator Rater Logo'
+                        />
                         <h5 className='top-logo-text'>GatorRater</h5>
                     </span>
                     <span>
