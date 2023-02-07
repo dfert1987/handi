d76390b4191e244916ee44e94559392101a62b49
David Fertitta <Davidj.fertitta@gmail.com>
2023-02-06 22:16:27 -0600

    menu bar work

--- src/Components/DefaultLayout.js
+++ src/Components/DefaultLayout.js
@@ -1,50 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Logo from '../Assets/Images/logogator.png';
+import Logo from '../Assets/Images/whitegator.png';
 import '../Styles/layout.css';
 
 const DefaultLayout = ({ children }) => {
+    const [collapsed, setCollapsed] = useState(false);
+
     const user = JSON.parse(localStorage.getItem('user'));
     const navigate = useNavigate();
     const userMenu = [
         {
             title: 'Home',
             onClick: () => navigate('/'),
-            icon: <i class='ri-home-3-line'></i>,
+            icon: <i className='ri-home-3-line'></i>,
+            path: '/',
         },
         {
             title: 'Discover',
             onClick: () => navigate('/find'),
-            icon: <i class='ri-map-2-line'></i>,
+            icon: <i className='ri-map-2-line'></i>,
+            path: '/find',
         },
         {
             title: 'Add Review',
             onClick: () => navigate('/review'),
-            icon: <i class='ri-edit-box-line'></i>,
+            icon: <i className='ri-edit-box-line'></i>,
+            path: '/review',
         },
         {
             title: 'Add Location',
             onClick: () => navigate('/location'),
-            icon: <i class='ri-map-pin-add-line'></i>,
+            icon: <i className='ri-map-pin-add-line'></i>,
+            path: '/location',
         },
         {
             title: 'Profile',
             onClick: () => navigate('/profile'),
-            icon: <i class='ri-profile-line'></i>,
+            icon: <i className='ri-profile-line'></i>,
+            path: '/profile',
         },
         {
             title: 'Logout',
-            onClick: () => localStorage.removItem('user'),
-            icon: <i class='ri-logout-box-r-line'></i>,
+            onClick: () => {
+                console.log(localStorage);
+                localStorage.removeItem('user');
+                navigate('/login');
+            },
+            icon: <i className='ri-logout-box-r-line'></i>,
+            path: '/login',
         },
     ];
+
     return (
         <div className='layout'>
             <div className='sidebar justify-content-between'>
-                <div className='menu'>
+                <div
+                    className='menu'
+                    style={{
+                        width: 150,
+                    }}>
                     {userMenu.map((item, index) => {
+                        const isActive = window.location.pathname === item.path;
                         return (
-                            <div className='menu-item'>
+                            <div
+                                className={`menu-item ${
+                                    isActive && 'active-menu-item'
+                                }`}
+                                key={index}
+                                onClick={item.onClick}>
                                 {item.icon}
 
                                 <span>{item.title}</span>
@@ -55,17 +78,27 @@ const DefaultLayout = ({ children }) => {
             </div>
             <div className='content'>
                 <div className='header'>
-                    <span className='top-bar-title'>
+                    <div className='top-bar-title'>
+                        {collapsed && (
+                            <i
+                                className='ri-menu-2-fill'
+                                onClick={() => setCollapsed(false)}></i>
+                        )}
+                        {!collapsed && (
+                            <i
+                                className='ri-close-line'
+                                onClick={() => setCollapsed(true)}></i>
+                        )}
                         <img
                             className='top-logo'
                             src={Logo}
                             alt='Gator Rater Logo'
                         />
                         <h5 className='top-logo-text'>GatorRater</h5>
-                    </span>
+                    </div>
                     <span>
                         <div className='logo-name'>
-                            <i class='ri-shield-user-line ri-2x '></i>
+                            <i className='ri-shield-user-line'></i>
                             <span className='username'>{user?.username}</span>
                         </div>
                     </span>
