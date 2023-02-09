5bba87fc6b89ecb28284fbf8c18b1154c31295c3
David Fertitta <Davidj.fertitta@gmail.com>
2023-02-09 08:34:20 -0600

    view options bar styled better

--- src/Views/Profile/ViewOptions.js
+++ src/Views/Profile/ViewOptions.js
@@ -6,29 +6,44 @@ export const ViewOptions = ({ user }) => {
         <div className='view-options-container'>
             <h4 className='viewOptions-header'>{user.username}'s Profile</h4>
             <div className='view-options'>
-                <div className='viewOption'>
-                    <i className='ri-profile-line optionIcon'></i>
-                    <p className='optionText'>Profile Overview</p>
+                <div className='option-divider'>
+                    <div className='viewOption'>
+                        <i className='ri-profile-line optionIcon'></i>
+                        <p className='optionText'>Profile Overview</p>
+                    </div>
+                    <hr />
                 </div>
-                <div className='viewOption'>
-                    <i className='ri-user-heart-line optionIcon'></i>
-                    <p className='optionText'>Friends</p>
+                <div className='option-divider'>
+                    <div className='viewOption'>
+                        <i className='ri-user-heart-line optionIcon'></i>
+                        <p className='optionText'>Friends</p>
+                    </div>
+                    <hr />
                 </div>
-                <div className='viewOption'>
-                    <i className='ri-star-line optionIcon'></i>
-                    <p className='optionText'>Reviews</p>
+                <div className='option-divider'>
+                    <div className='viewOption'>
+                        <i className='ri-star-line optionIcon'></i>
+                        <p className='optionText'>Reviews</p>
+                    </div>
+                    <hr />
                 </div>
-                <div className='viewOption'>
-                    <i className='ri-award-line optionIcon'></i>
-                    <p className='optionText'>Compliments</p>
+                <div className='option-divider'>
+                    <div className='viewOption'>
+                        <i className='ri-award-line optionIcon'></i>
+                        <p className='optionText'>Compliments</p>
+                    </div>
+                    <hr />
                 </div>
-                <div className='viewOption'>
-                    <img
-                        className='schneef option'
-                        alt='schneef nose'
-                        src={schneef}
-                    />
-                    <p className='optionText'>Schneefed In</p>
+                <div className='option-divider'>
+                    <div className='viewOption'>
+                        <img
+                            className='schneef option'
+                            alt='schneef nose'
+                            src={schneef}
+                        />
+                        <p className='optionText'>Schneefed In</p>
+                    </div>
+                    <hr />
                 </div>
             </div>
         </div>
