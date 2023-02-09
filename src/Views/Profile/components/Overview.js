b4be66b57e10c35e042e249dee7b802b79e51d2e
David Fertitta <Davidj.fertitta@gmail.com>
2023-02-09 14:55:10 -0600

    placeholder overview content in place

--- src/Views/Profile/components/Overview.js
+++ src/Views/Profile/components/Overview.js
@@ -2,7 +2,25 @@ import React from 'react';
 import '../../../Styles/Profile.css';
 
 const Overview = ({ user }) => {
-    return <>OVERVIEW</>;
+    return (
+        <>
+            <div className='friens-compliments-container'>
+                <h3 className='main-title friends-compliments'>
+                    Friend Requests and Compliments
+                </h3>
+                <p className='no-requests'>
+                    No new friend requests or compliments at this time.
+                </p>
+            </div>
+            <div className='activity-container'>
+                <h3 className='recent-title'>Recent Activity</h3>
+                <hr className='activity-divider' />
+                <div className='activities'>
+                    <p className='no-activities'>Do you even party?</p>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Overview;
