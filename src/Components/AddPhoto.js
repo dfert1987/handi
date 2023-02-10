8762b0e3c0b18439d3d25c61f49094a2f1ef0a18
David Fertitta <Davidj.fertitta@gmail.com>
2023-02-09 23:28:48 -0600

    upload image start

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { Button, message, Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 import '../Styles/ImageModal.css';
 
 const ImageModal = ({ user, setViewImageUpload }) => {
+    const [avatar, setAvatar] = useState();
+
+    const validateFileType = ({ type, name }, allowedTypes) => {
+        if (!allowedTypes) {
+            return true;
+        }
+        if (type) {
+            return allowedTypes.includes(type);
+        }
+    };
+
+    console.log(avatar);
+
+    const uploadProps = useMemo(
+        () => ({
+            beforeUpload: (file) => {
+                const isAllowedType = validateFileType(
+                    file,
+                    'image/png',
+                    'image/jpeg',
+                    'image/jpg'
+                );
+                if (!isAllowedType) {
+                    message.error(`${file.name} is not a valid file type`);
+                    return false;
+                }
+                if (avatar) {
+                    message.error(
+                        'Only one image upload allowed at a time. Remove previous upload below before choosing a new image,'
+                    );
+                    return false;
+                } else {
+                    setAvatar(file);
+                    return false;
+                }
+            },
+            onRemove: () => {
+                setAvatar();
+                return true;
+            },
+        }),
+        []
+    );
     return (
         <div className='img-modal-container'>
             <div className='modal-top'>
@@ -9,7 +54,11 @@ const ImageModal = ({ user, setViewImageUpload }) => {
                     className='ri-close-line closeModal'
                     onClick={() => setViewImageUpload(false)}></i>
             </div>
-            <div className='modal-body'></div>
+            <div className='modal-body'>
+                <Upload {...uploadProps} listType='picture' maxCount={1}>
+                    <Button icon={<UploadOutlined />}>Upload an Image</Button>
+                </Upload>{' '}
+            </div>
         </div>
     );
 };
